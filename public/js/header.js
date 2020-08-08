a211d1738369c80d2eaa21c81ec09038c5adfa6a
Chinmoy Chakraborty <chinmoy12c@gmail.com>
2020-08-08 14:47:00 +0530

    search bar dynamic

--- public/js/header.js
+++ public/js/header.js
@@ -4,13 +4,31 @@ searchCategory = () => {
         window.location.href = "/search?search=" + searchCategory;
 };
 
-window.onload = (e) => {
+setTimeout(() => {
     let searchBar = document.getElementById("searchInput");
-    console.log(searchBar);
-    searchBar.addEventListener("keydown", function(event){
-        alert("Asd");
-        if (event.keyCode == 13) {
-            searchCategory();
+    let searchSuggest = document.getElementById("searchSuggest");
+    let searchSuggestContainer = document.createElement("div");
+
+    let searchKeys = ["Tshirts", "Pants", "Jeans", "Blazzers"];
+
+    searchBar.addEventListener("keyup", (event) => {
+        searchSuggest.innerHTML = "";
+        searchSuggestContainer.innerHTML = "";
+        if (searchSuggestContainer.innerHTML == "")
+            searchSuggest.style.visibility = "hidden";
+        let textValue = searchBar.value;
+        if (textValue.length == 0)
+            return;
+
+        for (let keyword in searchKeys) {
+            if (searchKeys[keyword].toLowerCase().indexOf(textValue.toLowerCase()) >= 0) {
+                searchSuggestContainer.innerHTML += `<div class="searchItem">` + searchKeys[keyword] + `</div>`;
+            }
         }
+
+        if (searchSuggestContainer.innerHTML != "")
+            searchSuggest.style.visibility = "visible";
+
+        searchSuggest.appendChild(searchSuggestContainer);
     });
-};
+}, 1000);
